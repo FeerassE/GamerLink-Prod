bab08cd54347ff02154cc3180f375345c8f7d3b6
Feerass Ellid <feerasse@gmail.com>
2018-01-12 11:01:57 -0500

    added back button

--- client/src/components/NotFound.js
+++ client/src/components/NotFound.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import SearchBar from './SearchBar';
 
 const NotFound = () => {
     return (
         <div className="notFound">
-            <h1> User Not Found. </h1>
+           <Link to="/">Back </Link> <h1> User Not Found. </h1>
         </div>
     )
 }
